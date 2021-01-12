513337b12deee395b25bbb7eda6555aca3d48783
omt <omt@linmint2020>
2021-01-11 20:19:38 -0800

    Added Main.java file and refactor the class a little bit.

---
+++ src/main/java/omt/bmidemo20210111/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package omt.bmidemo20210111;
+
+/**
+ *
+ * @author omt
+ */
+public class Main {
+    /**
+     * This is the main entry point for the application!
+     * @param args 
+     */
+    public static void main(String[] args) {
+        System.out.println("--- BMI Demo (11 Jan 2021/Mon) ---");
+        BMIDemo demo = new BMIDemo();
+        demo.bmidemo();
+    }
+}
